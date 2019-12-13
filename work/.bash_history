1183b7be6e8968d34bc46e21e97540d8450278a7
n9d <39367157+n9d@users.noreply.github.com>
2019-12-13 13:01:59 +0900

    first commit

---
+++ work/.bash_history
@@ -0,0 +1,7 @@
+ls
+python sample.py 
+python3 sample.py 
+vi
+python3 sample2.py 
+python3 sample2.py 
+python3 sample.py 
---
+++ work/sample.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=options)
+driver.get('https://www.google.com')
+print(driver.title)
